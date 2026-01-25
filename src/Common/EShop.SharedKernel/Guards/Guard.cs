063545b1b39347cd3a7a848b3f6b39599c92b36d
Tomáš Starenko <starenkot@protonmail.com>
2026-01-25 11:21:52 +0100

    [00-00] style: add braces to if statements

--- src/Common/EShop.SharedKernel/Guards/Guard.cs
+++ src/Common/EShop.SharedKernel/Guards/Guard.cs
@@ -8,31 +8,43 @@ public static class Guard
             where T : class
         {
             if (value is null)
+            {
                 throw new ArgumentNullException(parameterName);
+            }
+
             return value;
         }
 
         public static string NullOrEmpty(string? value, string parameterName)
         {
             if (string.IsNullOrEmpty(value))
+            {
                 throw new ArgumentException("Value cannot be null or empty.", parameterName);
+            }
+
             return value;
         }
 
         public static int NegativeOrZero(int value, string parameterName)
         {
             if (value <= 0)
+            {
                 throw new ArgumentOutOfRangeException(
                     parameterName,
                     "Value must be greater than zero."
                 );
+            }
+
             return value;
         }
 
         public static decimal Negative(decimal value, string parameterName)
         {
             if (value < 0)
+            {
                 throw new ArgumentOutOfRangeException(parameterName, "Value cannot be negative.");
+            }
+
             return value;
         }
     }
