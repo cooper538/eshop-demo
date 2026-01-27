19572cf06962acdd5511366fa08bb1a51e7648bc
Tomáš Starenko <starenkot@protonmail.com>
2026-01-27 21:36:40 +0100

    [07-05] feat: add MassTransit CorrelationId filters for message tracing

---
+++ src/Common/EShop.Common/Correlation/MassTransit/CorrelationIdSendFilter.cs
@@ -0,0 +1,18 @@
+using MassTransit;
+
+namespace EShop.Common.Correlation.MassTransit;
+
+public sealed class CorrelationIdSendFilter<T> : IFilter<SendContext<T>>
+    where T : class
+{
+    public async Task Send(SendContext<T> context, IPipe<SendContext<T>> next)
+    {
+        var correlationId = CorrelationContext.Current?.CorrelationId ?? Guid.NewGuid().ToString();
+
+        context.Headers.Set(CorrelationIdConstants.MassTransitHeaderKey, correlationId);
+
+        await next.Send(context);
+    }
+
+    public void Probe(ProbeContext context) => context.CreateFilterScope("correlationIdSendFilter");
+}
