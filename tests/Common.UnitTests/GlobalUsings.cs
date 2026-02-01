75b4b15311d745bfc1a5e3f575a187bc95af5c96
Tomáš Starenko <starenkot@protonmail.com>
2026-02-01 11:56:47 +0100

    [00-00] meta: format GlobalUsings

--- tests/Common.UnitTests/GlobalUsings.cs
+++ tests/Common.UnitTests/GlobalUsings.cs
@@ -1,11 +1,6 @@
-﻿// Test framework
-// AutoFixture
-global using AutoFixture;
+﻿global using AutoFixture;
 global using AutoFixture.Xunit2;
-// Test fixtures
 global using EShop.Common.UnitTests.Fixtures;
-// Assertions
 global using FluentAssertions;
-// Mocking
 global using Moq;
 global using Xunit;
