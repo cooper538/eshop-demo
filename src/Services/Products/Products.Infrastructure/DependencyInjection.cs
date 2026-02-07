9e9da52f87d0b96f89b48cf7ab9ccb90a7df763b
Tomáš Starenko <starenkot@protonmail.com>
2026-02-07 21:09:27 +0100

    [15-01] fix: Register DomainEventDispatchInterceptor in Product service DI

--- src/Services/Products/Products.Infrastructure/DependencyInjection.cs
+++ src/Services/Products/Products.Infrastructure/DependencyInjection.cs
@@ -1,10 +1,13 @@
 ﻿using EShop.Common.Application.Data;
 using EShop.Common.Infrastructure.Data;
+using EShop.Common.Infrastructure.Data.Interceptors;
 using EShop.Common.Infrastructure.Extensions;
 using EShop.Products.Application.Data;
 using EShop.Products.Infrastructure.BackgroundJobs;
 using EShop.Products.Infrastructure.Data;
 using EShop.ServiceDefaults;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 
@@ -14,14 +17,56 @@ public static class DependencyInjection
 {
     public static IHostApplicationBuilder AddInfrastructure(this IHostApplicationBuilder builder)
     {
+        builder.Services.AddScoped<DomainEventDispatchInterceptor>();
+
         // Environment-aware database configuration
         if (builder.Environment.IsProduction())
         {
-            builder.AddNpgsqlDbContextAzure<ProductDbContext>(ResourceNames.Databases.Product);
+            var connectionString = builder.Configuration.GetConnectionString(
+                ResourceNames.Databases.Product
+            );
+
+            if (!string.IsNullOrWhiteSpace(connectionString))
+            {
+                connectionString = PostgresConnectionStringBuilder.EnsureSslMode(connectionString);
+
+                builder.Services.AddDbContext<ProductDbContext>(
+                    (sp, options) =>
+                    {
+                        options.UseNpgsql(
+                            connectionString,
+                            npgsqlOptions =>
+                            {
+                                npgsqlOptions.EnableRetryOnFailure(
+                                    maxRetryCount: 3,
+                                    maxRetryDelay: TimeSpan.FromSeconds(10),
+                                    errorCodesToAdd: null
+                                );
+                            }
+                        );
+                        options.AddInterceptors(
+                            sp.GetRequiredService<DomainEventDispatchInterceptor>()
+                        );
+                    }
+                );
+            }
         }
         else
         {
-            builder.AddNpgsqlDbContext<ProductDbContext>(ResourceNames.Databases.Product);
+            builder.Services.AddDbContext<ProductDbContext>(
+                (sp, options) =>
+                {
+                    var connectionString = builder.Configuration.GetConnectionString(
+                        ResourceNames.Databases.Product
+                    );
+                    options.UseNpgsql(connectionString);
+                    options.AddInterceptors(
+                        sp.GetRequiredService<DomainEventDispatchInterceptor>()
+                    );
+                }
+            );
+
+            builder.EnrichNpgsqlDbContext<ProductDbContext>();
         }
 
         builder.Services.AddScoped<IProductDbContext>(sp =>
